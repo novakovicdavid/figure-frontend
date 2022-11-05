094fc41a503026fbd54a45b5c7a46f175cd9b9c9
david <83370778+novakovicdavid@users.noreply.github.com>
2022-11-05 12:20:27 +0100

    Prevent multiple items from being highlighted in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,40 +2,44 @@ import {Container, Nav, Navbar} from "react-bootstrap";
 import {LinkContainer} from 'react-router-bootstrap'
 import {fbAuth} from "../services/firebase";
 import {useAuthContext} from "../contexts/authContext";
+import {useState} from "react";
 
 export function Header() {
     const {user, loading} = useAuthContext()
+    const [activeLink, setActiveLink] = useState("");
     return (
         <Navbar bg="light" expand="md">
             <Container>
-                <LinkContainer to={"/"}><Navbar.Brand>Figure</Navbar.Brand></LinkContainer>
+                <LinkContainer to={"/"} onClick={() => setActiveLink("none")}>
+                    <Navbar.Brand>Figure</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <LinkContainer to={"/browse"}>
+                        <LinkContainer to={"/browse"} onClick={() => setActiveLink("browse")} active={activeLink === "browse"}>
                             <Nav.Link>Browse</Nav.Link>
                         </LinkContainer>
-                        <LinkContainer to={"/about"}>
+                        <LinkContainer to={"/about"} onClick={() => setActiveLink("about")} active={activeLink === "about"}>
                             <Nav.Link>About</Nav.Link>
                         </LinkContainer>
                     </Nav>
                     <Nav>
                         {
                             !loading && !user &&
-                            <LinkContainer to={"/register"}>
+                            <LinkContainer to={"/register"} onClick={() => setActiveLink("register")} active={activeLink === "register"}>
                                 <Nav.Link>Sign up</Nav.Link>
                             </LinkContainer>
 
                         }
                         {
                             user &&
-                            <LinkContainer to={"/upload"}>
+                            <LinkContainer to={"/upload"} onClick={() => setActiveLink("upload")} active={activeLink === "upload"}>
                                 <Nav.Link>Upload</Nav.Link>
                             </LinkContainer>
                         }
                         {
                             user &&
-                            <LinkContainer to={"/profile/" + user.uid}>
+                            <LinkContainer to={"/profile/" + user.uid} onClick={() => setActiveLink("profile")} active={activeLink === "profile"}>
                                 <Nav.Link>My Profile</Nav.Link>
                             </LinkContainer>
                         }
