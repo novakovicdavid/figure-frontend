bbd4cd9ff72d13d096f04588b37974e28648dbdb
david <83370778+novakovicdavid@users.noreply.github.com>
2022-11-06 15:45:21 +0100

    Spinner & disable button during upload

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,42 +1,51 @@
 import Form from 'react-bootstrap/Form';
-import {Button} from "react-bootstrap";
+import {Button, Spinner} from "react-bootstrap";
 import {useState} from "react";
 import {ref, uploadBytes} from "firebase/storage";
-import {fbAuth, fbFirestore, fbStorage} from "../services/firebase";
+import {fbFirestore, fbStorage} from "../services/firebase";
 import {v4 as uuidv4} from 'uuid';
 import {doc, setDoc} from "firebase/firestore";
 import {serverTimestamp} from "firebase/firestore"
+import {useAuthContext} from "../contexts/authContext";
+import {useNavigate} from "react-router-dom";
 
-function handleUpload(event, title, description, file) {
+function handleUpload(event, title, description, file, useruid, navigate) {
     event.preventDefault();
     const uuid = uuidv4();
     if (file === null) return;
 
     const metadata = {
         customMetadata: {
-            'user': fbAuth.currentUser.uid
+            'user': useruid
         }
     }
     const storageRef = ref(fbStorage, 'figures/' + uuid);
     uploadBytes(storageRef, file, metadata).then(() => {
-        console.log('Uploaded a blob or file!');
         setDoc(doc(fbFirestore, "figures", uuid), {
             creation: serverTimestamp(),
             description: description,
             title: title,
-            user: fbAuth.currentUser.uid
+            user: useruid
         }).then(() => {
-            console.log('Created database entry')
+            navigate("/figure/" + uuid);
         })
     });
 }
 
 export function UploadPage() {
-    const [file, setFile] = useState(null)
-    const [title, setTitle] = useState(null)
-    const [description, setDescription] = useState(null)
+    const [file, setFile] = useState(null);
+    const [title, setTitle] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [submitButtonActive, setSubmitButtonActive] = useState(true);
+    const {user} = useAuthContext();
+    const navigate = useNavigate();
+
+
     return (
-        <Form onSubmit={(e) => handleUpload(e, title, description, file)}>
+        <Form onSubmit={(e) => {
+            handleUpload(e, title, description, file, user.uid, navigate);
+            setSubmitButtonActive(false);
+        }}>
             <Form.Group controlId="formFile" className="mb-3">
                 <Form.Label>Default file input example</Form.Label>
                 <Form.Control type="file" onChange={(e) => setFile(e.target.files[0])}
@@ -50,7 +59,11 @@ export function UploadPage() {
                 <Form.Label>Description</Form.Label>
                 <Form.Control type={"text"} onChange={(e) => setDescription(e.target.value)}/>
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" disabled={submitButtonActive}>
+                {
+                    submitButtonActive &&
+                    <Spinner as={"span"} animation={"border"} size={"sm"} className={"me-1"}/>
+                }
                 Submit
             </Button>
         </Form>
