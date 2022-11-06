80422857a34a251821805d6d433f294be29fb398
david <83370778+novakovicdavid@users.noreply.github.com>
2022-11-06 10:14:17 +0100

    Figures component

---
+++ src/components/Figures.js
@@ -0,0 +1,32 @@
+import {Card, Col, Container, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
+
+function Figure(props) {
+    const {id, url, title} = props;
+
+    return(
+        <Row>
+            <Col>
+                <Link to={'/figure/' + id}>
+                    <Card className={"mb-4"}>
+                        <Card.Img variant={"top"} src={url}/>
+                        <Card.Header as={"h5"}>{title}</Card.Header>
+                    </Card>
+                </Link>
+            </Col>
+        </Row>
+    )
+}
+
+export function Figures(props) {
+    const {figures} = props;
+    return (
+        <Container>
+            {
+                figures.map((figure) =>
+                    <Figure id={figure.id} url={figure.url} title={figure.data().title}/>
+                )
+            }
+        </Container>
+    )
+}
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,7 +1,8 @@
-import {Link, useLoaderData, useParams} from "react-router-dom";
+import {useLoaderData, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import {fetchNextFigures} from "../utilities/FigureFetching";
+import {Figures} from "../components/Figures";
 
 function getAndAppendFigures(figures, setFigures, setCountOfNewFigures, useruid, collectionRef, queryOrder, queryMaxItems) {
     const lastDoc = figures[figures.length - 1];
@@ -26,42 +27,26 @@ export function ProfilePage(props) {
     }, [figuresFromLoader]);
 
     return (
-        <>
-            <div id={"scroller"} className={"overflow-scroll"}>
-                {
-                    figures.length > 0 &&
-                    <InfiniteScroll
-                        dataLength={figures.length}
-                        next={() => {
-                            getAndAppendFigures(figures, setFigures, setCountOfNewFigures, useruid, collectionRef, queryOrder, queryMaxItems)
-                        }}
-                        hasMore={countOfNewFigures === limitOfNewItems}
-                        loader={<h4>Loading...</h4>}
-                        endMessage={
-                            <p style={{textAlign: 'center'}}>
-                                <b>Yay! You have seen it all</b>
-                            </p>
-                        }
-                        scrollableTarget={"scroller"}
-                    >
-                        {
-                            figures.map((figure) => {
-                                return (
-                                    <div style={{height: "500px"}} key={figure.id}>
-                                        <Link to={'/figure/' + figure.id}>
-                                            {
-                                                <img style={{height: "100px"}} src={figure.url}/>
-                                            }
-                                        </Link>
-                                        <p>{figure.data().title}</p>
-                                        <p>{figure.data().description}</p>
-                                    </div>
-                                )
-                            })
-                        }
-                    </InfiniteScroll>
-                }
-            </div>
-        </>
+        <div id={"scroller"} className={"overflow-scroll"} style={{width: "1000px", justifySelf: "center"}}>
+            {
+                figures.length > 0 &&
+                <InfiniteScroll
+                    dataLength={figures.length}
+                    next={() => {
+                        getAndAppendFigures(figures, setFigures, setCountOfNewFigures, useruid, collectionRef, queryOrder, queryMaxItems)
+                    }}
+                    hasMore={countOfNewFigures === limitOfNewItems}
+                    loader={<h4>Loading...</h4>}
+                    endMessage={
+                        <p style={{textAlign: 'center'}}>
+                            <b>Yay! You have seen it all</b>
+                        </p>
+                    }
+                    scrollableTarget={"scroller"}
+                >
+                    <Figures figures={figures}/>
+                </InfiniteScroll>
+            }
+        </div>
     )
 }
