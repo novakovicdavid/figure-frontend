0e9e7554ac3a624a2ead455169e0867a99c186fb
david <83370778+novakovicdavid@users.noreply.github.com>
2022-11-08 21:30:11 +0100

    Fix navbar not collapsing when pressing a nav item

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,20 +9,30 @@ export function Header() {
     const {username, loading} = useAuthContext()
     const [activeLink, setActiveLink] = useState("");
     const [showUploadModal, setShowUploadModal] = useState(false);
+    const [expanded, setExpanded] = useState(false);
     return (
-        <Navbar bg="light" expand="md" collapseOnSelect={true}>
+        <Navbar bg="light" expand="md" collapseOnSelect={true} expanded={expanded}>
             <Container>
-                <LinkContainer to={"/"} onClick={() => setActiveLink("none")}>
+                <LinkContainer to={"/"} onClick={() => {
+                    setActiveLink("none");
+                    setExpanded(false);
+                }}>
                     <Navbar.Brand>Figure</Navbar.Brand>
                 </LinkContainer>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)}/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <LinkContainer to={"/browse"} onClick={() => setActiveLink("browse")}
+                        <LinkContainer to={"/browse"} onClick={() => {
+                            setActiveLink("browse");
+                            setExpanded(false);
+                        }}
                                        active={activeLink === "browse"}>
                             <Nav.Link>Browse</Nav.Link>
                         </LinkContainer>
-                        <LinkContainer to={"/about"} onClick={() => setActiveLink("about")}
+                        <LinkContainer to={"/about"} onClick={() => {
+                            setActiveLink("about");
+                            setExpanded(false);
+                        }}
                                        active={activeLink === "about"}>
                             <Nav.Link>About</Nav.Link>
                         </LinkContainer>
@@ -31,11 +41,17 @@ export function Header() {
                         {
                             !loading && !username &&
                             <>
-                                <LinkContainer to={"/login"} onClick={() => setActiveLink("login")}
+                                <LinkContainer to={"/login"} onClick={() => {
+                                    setActiveLink("login");
+                                    setExpanded(false);
+                                }}
                                                active={activeLink === "login"} className={"me-3"}>
                                     <Nav.Link>Login</Nav.Link>
                                 </LinkContainer>
-                                <LinkContainer to={"/register"} onClick={() => setActiveLink("register")}
+                                <LinkContainer to={"/register"} onClick={() => {
+                                    setActiveLink("register");
+                                    setExpanded(false);
+                                }}
                                                active={activeLink === "register"} className={"p-0"}>
                                     <Nav.Link>
                                         <Button>Sign up</Button>
@@ -46,19 +62,29 @@ export function Header() {
                         {
                             username &&
                             <Nav.Link onClick={() => {
+                                setActiveLink("upload");
+                                setExpanded(false);
                                 setShowUploadModal(true);
-                            }}>Upload</Nav.Link>
+                            }}>
+                                Upload
+                            </Nav.Link>
                         }
                         {
                             username &&
-                            <LinkContainer to={"/profile/" + username} onClick={() => setActiveLink("profile")}
+                            <LinkContainer to={"/profile/" + username} onClick={() => {
+                                setActiveLink("profile");
+                                setExpanded(false);
+                            }}
                                            active={activeLink === "profile"}>
                                 <Nav.Link>Profile ({username})</Nav.Link>
                             </LinkContainer>
                         }
                         {
                             username &&
-                            <Nav.Link onClick={() => fbAuth.signOut()}>Logout</Nav.Link>
+                            <Nav.Link onClick={() => {
+                                fbAuth.signOut();
+                                setExpanded(false);
+                            }}>Logout</Nav.Link>
                         }
                     </Nav>
                 </Navbar.Collapse>
