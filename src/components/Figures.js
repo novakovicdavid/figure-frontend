8412ebd9985f136e6a8a9b088bc44ffcf83f9738
david <83370778+novakovicdavid@users.noreply.github.com>
2022-11-08 20:52:51 +0100

    Show user under each figer

--- src/components/Figures.js
+++ src/components/Figures.js
@@ -2,16 +2,18 @@ import {Card, Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 function Figure(props) {
-    const {id, url, title, width, height} = props;
+    const {id, url, title, width, height, user} = props;
     return (
         <Row>
             <Col>
-                <Link to={'/figure/' + id}>
-                    <Card className={"mb-4"}>
+                <Card className={"mb-4"}>
+                    <Link to={'/figure/' + id}>
                         <Card.Img variant={"top"} src={url} style={{aspectRatio: width + ' / ' + height}}/>
                         <Card.Header as={"h5"}>{title}</Card.Header>
-                    </Card>
-                </Link>
+                    </Link>
+                    <p className={"ms-3 mt-1 mb-1"}>{user}</p>
+
+                </Card>
             </Col>
         </Row>
     )
@@ -19,12 +21,13 @@ function Figure(props) {
 
 export function Figures(props) {
     const {figures} = props;
+    console.log(figures);
     return (
         <Container>
             {
                 figures.map((figure) =>
                     <Figure id={figure.id} url={figure.url} title={figure.data().title} width={figure.data().sizex}
-                            height={figure.data().sizey} key={figure.id}/>
+                            height={figure.data().sizey} user={figure.data().user} key={figure.id}/>
                 )
             }
         </Container>
