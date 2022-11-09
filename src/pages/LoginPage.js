feb56419402dd9c40fa2e56093502c1ef997e8e1
david <83370778+novakovicdavid@users.noreply.github.com>
2022-11-09 18:51:54 +0100

    Change login page redirect as user to profile

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,13 +4,13 @@ import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 
 export function LoginPage() {
-    const {user} = useAuthContext();
+    const {username} = useAuthContext();
     const navigate = useNavigate();
     useEffect(() => {
-        if (user) {
-            navigate('/')
+        if (username) {
+            navigate('/profile/' + username)
         }
-    }, [user])
+    }, [username])
     return(
         <Login/>
     )
