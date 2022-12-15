72fd53450508defbbb57166d029f64dca9da30fe
david <83370778+novakovicdavid@users.noreply.github.com>
2022-12-15 20:53:11 +0100

    Homepage fade-in once

--- src/components/FigureTease.js
+++ src/components/FigureTease.js
@@ -1,22 +1,22 @@
-import {Button, Col, Row} from "react-bootstrap";
+import {Button, Col} from "react-bootstrap";
 import {LinkContainer} from 'react-router-bootstrap'
 
 export function FigureTease(props) {
-    const {figures} = props;
+    const {figures, style} = props;
     return (
-        <Row className={"gap-4 justify-content-center position-relative"}>
+        <>
             {
                 figures.map((figure) =>
-                    <Col key={figure.id} className={"p-0"} xs={12} sm={3} md={2} style={{objectFit: "cover"}}>
+                    <Col key={figure.id} className={"p-0"} xs={12} sm={3} md={2} style={{objectFit: "cover", ...style}}>
                         <img src={figure.url} style={{width: "100%", height: "100%", objectFit: "cover", aspectRatio: figure.data().sizex + " / " + figure.data().sizey}}/>
                     </Col>
                 )
             }
-            <div className={"d-flex justify-content-center align-items-end position-absolute bottom-0"} style={{height: "500px", backgroundImage: "linear-gradient(transparent, white)"}}>
+            <div className={"d-flex justify-content-center align-items-end position-absolute bottom-0"} style={{height: "100%", backgroundImage: "linear-gradient(transparent, white 90%)"}}>
                 <LinkContainer to={"/register"}>
                     <Button className={"mb-5"}>Join Figure</Button>
                 </LinkContainer>
             </div>
-        </Row>
+        </>
     )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,9 @@
 import {Button, Col, Container, Image, Row} from "react-bootstrap";
 import {LinkContainer} from 'react-router-bootstrap'
 import {useLoaderData} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useEffect, useLayoutEffect, useMemo, useState} from "react";
 import {FigureTease} from "../components/FigureTease";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import {useInView} from "react-intersection-observer";
 
 export function HomePage() {
     const {totalFiguresPromise, totalUsersPromise, latestFiguresPromise} = useLoaderData();
@@ -12,7 +11,6 @@ export function HomePage() {
     const [totalUsers, setTotalUsers] = useState("...")
     const [latestFigures, setLatestFigures] = useState();
     useEffect(() => {
-        AOS.init();
         totalFiguresPromise.then((total) => {
             setTotalFigures(total);
         });
@@ -22,13 +20,36 @@ export function HomePage() {
         latestFiguresPromise.then((urls) => {
             setLatestFigures(urls);
         })
-    }, [])
+    }, []);
+
+    // Fade in Landing part
+    const [landingStyle, setLandingStyle] = useState({visibility: 'hidden', opacity: 0});
+    useEffect(() => {
+        setLandingStyle({visibility: 'visible', opacity: 1, transform: 'translate(0, 0)'})
+    }, []);
+
+    // Fade in Figure teaser
+    const [teaserStyle, setTeaserStyle] = useState({visibility: 'hidden', opacity: 0});
+    const {ref, inView} = useInView({
+        threshold: 0.2,
+    });
+    useLayoutEffect(() => {
+        console.log(inView);
+        if (teaserStyle.opacity === 0 && inView) setTeaserStyle({visibility: 'visible', opacity: 1, transform: 'translate(0, 0)'})
+    }, [inView]);
+
 
     return (
         <Container fluid>
             <Row className={"text-bg-light p-4 pt-5 p-sm-5"}
-                 style={{display: "flex", flexDirection: "column", placeItems: "center"}}>
-                <Col className={"p-0 pt-5"} style={{maxWidth: "70em"}} data-aos={"fade-up"}>
+                 style={{display: "flex", flexDirection: "column", placeItems: "center", position: "relative"}}>
+                <Col className={"p-0 pt-0 pt-sm-5"}
+                     style={{
+                         maxWidth: "70em",
+                         transition: 'visibility 0.2s linear, opacity 0.2s linear, transform 0.4s ease',
+                         transform: 'translate(0, 5em)',
+                         ...landingStyle
+                     }}>
                     <Row>
                         <Col md={8} lg={8} xxl={8}>
                             <Row>
@@ -58,27 +79,32 @@ export function HomePage() {
                         </Col>
                         <Col md={4} lg={4} xxl={4} style={{display: "flex", alignItems: "center"}}>
                             <div style={{maxWidth: "100%"}}>
-                                <Image fluid src={"https://i.imgur.com/E2tqE3F.png"}
-                                       style={{minHeight: "18.75em", minWidth: "18.75em"}}/>
+                                <Image fluid src={"https://i.imgur.com/E2tqE3F.png"}/>
                             </div>
                         </Col>
                     </Row>
                 </Col>
             </Row>
-            <div className={"p-4 p-sm-5"} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+            <Row className={"p-4 p-sm-5"} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                 {
                     !latestFigures &&
                     <div style={{height: "47.375em"}}/>
                 }
                 {
                     latestFigures &&
-                    <div className={"p-0"} style={{maxWidth: "70em", width: "100%"}} data-aos={"fade-up"}>
+                    <Row className={"p-0 gap-4 justify-content-center position-relative"}
+                         style={{
+                             maxWidth: "70em",
+                             width: "100%",
+                             transition: 'visibility 0.2s linear, opacity 0.2s linear, transform 0.4s ease',
+                             transform: 'translate(0, 5em)',
+                             ...teaserStyle
+                         }} ref={ref}>
                         <h2 className={"p-0 mb-4 mt-4"}>Latest Figures</h2>
                         <FigureTease figures={latestFigures}/>
-                    </div>
+                    </Row>
                 }
-
-            </div>
+            </Row>
             <small>(*) Until I take it down, probably friday evening.</small>
             {/*Inspired by https://saaslandingpage.com/templates/creative/*/}
         </Container>
